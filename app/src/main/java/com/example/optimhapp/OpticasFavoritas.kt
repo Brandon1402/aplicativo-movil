b1c2c8b1752537a280efecd2fb894b8c7ee5ba7b
Brandon <brandonramirez1193@hotmail.com>
2022-11-25 03:54:46 -0500

    se realizan detalles de funconal de boton y se agregan opticas

--- app/src/main/java/com/example/optimhapp/OpticasFavoritas.kt
+++ app/src/main/java/com/example/optimhapp/OpticasFavoritas.kt
@@ -4,19 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class OpticasFavoritas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_opticas_favoritas)
         iniEvent()
+        agregarOpti()
 
     }
 
     fun iniEvent() {
 
-        val Button = findViewById<Button>(R.id.button32)
-        Button.setOnClickListener {
+        val ImageButton = findViewById<ImageButton>(R.id.imageButton)
+        ImageButton.setOnClickListener {
+            val intent = Intent(this, Menu::class.java)
+            startActivity(intent)
+        }
+    }
+    fun agregarOpti() {
+        val button = findViewById<Button>(R.id.button32)
+        button.setOnClickListener {
             val intent = Intent(this, AgregarOptica::class.java)
             startActivity(intent)
         }
