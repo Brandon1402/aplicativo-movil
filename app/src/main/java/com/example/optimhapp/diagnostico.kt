762c4026948d6661944914f8339fe5504f4b0082
Brandon <brandonramirez1193@hotmail.com>
2022-11-24 20:01:16 -0500

    se hace ajustes de tamaños de letras y vista de diagnostico

--- app/src/main/java/com/example/optimhapp/diagnostico.kt
+++ app/src/main/java/com/example/optimhapp/diagnostico.kt
@@ -4,18 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
-class pregunta7 : AppCompatActivity() {
+class diagnostico : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pregunta7)
+        setContentView(R.layout.activity_diagnostico)
         iniEvent()
     }
     fun iniEvent() {
 
-        val Button = findViewById<Button>(R.id.button26)
-        Button.setOnClickListener {
-            val intent = Intent(this, diagnostico::class.java)
+        val imageView = findViewById<ImageView>(R.id.imageView50)
+        imageView.setOnClickListener {
+            val intent = Intent(this, Menu::class.java)
             startActivity(intent)
         }
     }
