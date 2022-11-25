b4bb43cd32873494c5c4b8abf52d1cb776a2eb3c
Brandon <brandonramirez1193@hotmail.com>
2022-11-24 23:37:13 -0500

    se hace logica para tomar personalizacion

--- app/src/main/java/com/example/optimhapp/CreacCuenta.kt
+++ app/src/main/java/com/example/optimhapp/CreacCuenta.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class CreacCuenta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +17,10 @@ class CreacCuenta : AppCompatActivity() {
 
         val Button = findViewById<Button>(R.id.button8)
         Button.setOnClickListener {
-            val intent = Intent(this, InicioSesion::class.java)
-            startActivity(intent)
+            var nombre = findViewById<EditText>(R.id.editTextTextPersonName2)
+            var enviar = Intent(this,Instrucciones::class.java)
+            enviar.putExtra("datos", nombre.text.toString())
+            startActivity(enviar)
         }
     }
     fun cuentaCre() {
--- app/src/main/java/com/example/optimhapp/Instrucciones.kt
+++ app/src/main/java/com/example/optimhapp/Instrucciones.kt
@@ -4,12 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class Instrucciones : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_instrucciones)
         iniEvent()
+        val bundle = intent.extras
+        val dato = bundle?.getString("datos")
+        val reco = findViewById<TextView>(R.id.textView29)
+        reco.text = "¡Bienvenid@, " + dato.toString() + "!"
     }
     fun iniEvent() {
 
